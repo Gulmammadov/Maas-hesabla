b3b3516129dd12f16fac24e24c5c9d70d42913db
Fuad Gulmammadov <fgulmemmedov1@std.qu.edu.az>
2018-01-18 12:33:50 +0400

    initial commit

---
+++ 20170927Win_MaasHesabla/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace _20170927Win_MaasHesabla
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Hesabla = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_Ad = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_ay = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_Maas = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_Hesabla
+            // 
+            this.btn_Hesabla.Location = new System.Drawing.Point(88, 107);
+            this.btn_Hesabla.Name = "btn_Hesabla";
+            this.btn_Hesabla.Size = new System.Drawing.Size(75, 23);
+            this.btn_Hesabla.TabIndex = 0;
+            this.btn_Hesabla.Text = "Hesabla";
+            this.btn_Hesabla.UseVisualStyleBackColor = true;
+            this.btn_Hesabla.Click += new System.EventHandler(this.btn_Hesabla_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ad:";
+            // 
+            // txt_Ad
+            // 
+            this.txt_Ad.Location = new System.Drawing.Point(63, 32);
+            this.txt_Ad.Name = "txt_Ad";
+            this.txt_Ad.Size = new System.Drawing.Size(100, 20);
+            this.txt_Ad.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ay:";
+            // 
+            // txt_ay
+            // 
+            this.txt_ay.Location = new System.Drawing.Point(63, 58);
+            this.txt_ay.Name = "txt_ay";
+            this.txt_ay.Size = new System.Drawing.Size(100, 20);
+            this.txt_ay.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Maaş:";
+            // 
+            // txt_Maas
+            // 
+            this.txt_Maas.Location = new System.Drawing.Point(63, 81);
+            this.txt_Maas.Name = "txt_Maas";
+            this.txt_Maas.Size = new System.Drawing.Size(100, 20);
+            this.txt_Maas.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(54, 187);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(189, 222);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txt_Maas);
+            this.Controls.Add(this.txt_ay);
+            this.Controls.Add(this.txt_Ad);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_Hesabla);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Hesabla;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_Ad;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_ay;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_Maas;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ 20170927Win_MaasHesabla/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _20170927Win_MaasHesabla
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Hesabla_Click(object sender, EventArgs e)
+        {
+            float netice;
+            netice = float.Parse(txt_ay.Text)*float.Parse(txt_Maas.Text);
+            MessageBox.Show("Sizin alacağınız maaş = " + netice.ToString());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float a = 85;
+            string c = "95";
+            int b;
+            b = (int)a;
+            b = Int32.Parse(c);
+            MessageBox.Show(b.ToString());
+        }
+    }
+}
